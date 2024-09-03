611814595c31a39d15b3385b91bd551261c168aa
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-03 14:45:08 -0300

    lista de armas, vida e classe de armadura do warrior criada

--- warrior.py
+++ warrior.py
@@ -1,4 +1,9 @@
 import random
 
 # Arma, dado, dano
-armas = [[]]
+armas = [['Espada Curta', 1, 6],
+         ['Espada Grande', 2 , 6],
+         ['Espada Longa', 1, 10]]
+
+armadura = 16
+vida = 10
