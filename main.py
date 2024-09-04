91ee8b182835ac5fc484d18fc4e10ec9b75b6e8d
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-04 14:02:58 -0300

    Combate sendo implementado

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import easy
 escolha_arma = None
 
 
-
-''' Definindo sua classe '''
+# Definindo sua classe
 op_classes = ['guerreiro']
 escolha_classe = None
 
@@ -19,7 +18,7 @@ while escolha_classe not in op_classes:
 print(f'Sua classe será {escolha_classe}')
 
 
-''' Definindo seus atributos '''
+# Definindo seus atributos
 def exibir_atributos(atributos: dict, pontos_livres: int) -> str:
     print('---Vamos definir seus atributos---')
     print(f'Você possui {pontos_livres} para distribuir nesses atributos')
@@ -35,7 +34,7 @@ def escolher_atributo(atributos: dict) -> str:
             print('Opção incorreta')
     return escolha_atributos
 
-def escolher_acao() -> str:
+def escolher_add_remove() -> str:
     remover_adicionar = None
     while remover_adicionar not in ['remover', 'adicionar']:
         remover_adicionar = input('Você quer remover ou adicionar pontos? ').lower()
@@ -76,7 +75,7 @@ def main_atr() -> dict:
         escolha_atributos = escolher_atributo(atributos)
         
         if escolha_atributos != 'sair':
-            acao = escolher_acao()
+            acao = escolher_add_remove()
             
             if acao == 'adicionar':
                 pontos_livres = adicionar_pontos(atributos, escolha_atributos, pontos_livres)
@@ -97,6 +96,10 @@ while escolha_arma not in warrior.armas:
     print(f'Espada Longa | 1d10')
     escolha_arma = input('Insira o nome da sua arma: ').title()
 
+if escolha_arma in warrior.armas:
+    arma = warrior.armas[escolha_arma]
+print(arma)
+
 # Comece o combate
 
 print('---Que o combate comece---')
@@ -104,29 +107,53 @@ def escolher_easy() -> list:
     inimigo = easy.randomizar_inimigo()
     return inimigo
 
-def iniciativa(destreza: int) -> int:
+def rolagem_iniciativa(destreza: int) -> int:
     des_calculada = (destreza - 10) / 2
     rolagem = random.randint(1, 20)
     iniciativa = des_calculada + rolagem
     return iniciativa
 
-def iniciativa_inimigo(destreza_inimigo: int) -> int:
+def rolagem_iniciativa_inimigo(destreza_inimigo: int) -> int:
     rolagem = random.randint(1, 20)
     iniciativa = rolagem + destreza_inimigo
     return iniciativa
 
-def primeira_acao(iniciativa_player: int, iniciativa_inimigo: int) -> str:
+def primeiro(iniciativa_player: int, iniciativa_inimigo: int) -> str:
     if iniciativa_player >= iniciativa_inimigo:
         primeira_acao = 'player'
     else:
         primeira_acao = 'inimigo'
     return primeira_acao
 
-def main_acao():
-    escolher_easy()
-    iniciativa(atributos['destreza'])
-
-
-print('---Qual ataque você quer fazer?')
-print(f'[1] Atacar com {escolha_arma}')
-escolha_ataque = int(input(': '))
+def atacar(forca: int, armadura: int, dado: int, dano: int) -> int:
+    ataque = random.randint(1, 20) + forca
+    print(f'Seu ataque foi {ataque}')
+    if ataque >= armadura:
+        dano_total = None
+        for n in range(dado):
+            dano = random.randint(1, dano) + forca
+            dano_total += dano
+        print(f'Seu dano total foi: {dano_total}')    
+        return dano_total
+    else:
+        print('Seu ataque errou')
+        return 0
+    
+def primeiro_acao():
+    inimigo = escolher_easy()
+    print(f'Seu inimigo é {inimigo[1]}')
+    print('Vamos rolar as iniciativas')
+    iniciativa_player = rolagem_iniciativa(atributos['destreza'])
+    print(f'Sua iniciativa foi: {iniciativa_player}')
+    iniciativa_inimigo = rolagem_iniciativa_inimigo(inimigo[5[2]])
+    print(f'A iniciativa do {inimigo[1]} foi: {iniciativa_inimigo}')
+    primeira_acao = primeiro(iniciativa_player, iniciativa_inimigo)
+    if primeira_acao == 'player':
+        escolher_acao = None
+        while escolher_acao not in [1, 2]:
+            print('Você começa!')
+            print('[1] Atacar')
+            print('[2] Fugir')
+            escolher_acao = int(input('Escolha sua ação: '))
+        if primeira_acao == 1:
+            atacar(atributos['força'], inimigo[3], )
