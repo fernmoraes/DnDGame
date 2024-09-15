bbdd57f8f4abf31344a5775a4a4abbc2b4d1e2df
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-15 16:37:03 -0300

    inimigos easy adaptados para novo estilo de luta

--- easy.py
+++ easy.py
@@ -1,13 +1,13 @@
 import random
 
-# [Nome, vida, armadura,[dado, dano, bonus], [for, des, cons, int,sab, car]] 
-enemy_Easy_List = [['Goblin', 7, 15, [1, 6, 2], [-1, 2, 0, 0, -1, -1]], 
-              ['Kobold', 5, 12, [1, 4, 2], [-2, 2, -1, -1, -2, -1]], 
-              ['Esqueleto', 13, 13, [1, 6, 2], [0, 2, 2, -2, -1, -3]],
-              ['Orcs', 15, 13, [1, 12, 3], [3, 1, 3, -2, 0, 0]], 
-              ['Lobos', 11, 13, [2, 4, 2], [1, 2, 1, -4, 1, -2]], 
-              ['Zumbis', 22, 8, [1, 6, 1], [1, -2, 3, -4, -2, -3]], 
-              ['Aranha Gigantes', 26, 14, [2, 8, 3], [2, 3, 1, -4, 0, -3]]]
+# [Nome, vida, armadura,[dado, dano, bonus], [strength, speed, body, magic]] 
+enemy_Easy_List = [['Goblin', 7, 15, [1, 6, 2], [-1, 2, 0, 0,]], 
+              ['Kobold', 5, 12, [1, 4, 2], [-2, 2, -1, -1,]], 
+              ['Esqueleto', 13, 13, [1, 6, 2], [0, 2, 2, -2,]],
+              ['Orcs', 15, 13, [1, 12, 3], [3, 1, 3, -2,]], 
+              ['Lobos', 11, 13, [2, 4, 2], [1, 2, 1, -4,]], 
+              ['Zumbis', 22, 8, [1, 6, 1], [1, -2, 3, -4,]], 
+              ['Aranha Gigantes', 26, 14, [2, 8, 3], [2, 3, 1, -4,]]]
 
 # [Nome, tipo, quantidade, [dado, quantidade de cura]]
 easy_Loot_List = [['Cura Pequena', 'cura', 1, [1, 8]],
